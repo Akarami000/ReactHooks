00a8e3255eb473cbd5341db6ba82732b9cd44f4f
Akarami000 <45476716+Akarami000@users.noreply.github.com>
2024-08-30 02:05:23 +0530

    Day2 use of key

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 step by step use of react-hooks lessons Update 
 
 - 1  Day one : We learn about, how to prevent from re-rendering and how to use React.memo to prevent re-rendering.
-- 2 Day two :
+- 2 Day two : We learn the why adding unique key is necessary to add in Component 
--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/////////////The bellow code is about saving code from re-rendering
 
+      //       0 headComponent
+      //      / \
+//child N    0   0
+      //   / | \
+      //  0  0  0  => *make sure to render child node to reduce re-rendering 
+
+// import {useState} from 'react'; 
+
+// function App() {
+  
+
+//   return (
+//     <div>
+//      <HeaderFunction/>
+//      <Header title={"My name is Akshay"}></Header>
+
+//     </div>
+//   )
+// }
+
+// function HeaderFunction(){
+//   const [name,setName]= useState(" My name is Akshay")
+//   function UpdateName(){
+//     setName("My name is "+ Math.random());
+//   }
+//   return <div>
+// <button onClick={()=>{UpdateName()}} >Click here to change the title</button>
+// <Header title={name}></Header>
+//   </div>
+// }
+// function Header({title}){
+//   return (
+//   <div>
+//     {title}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+////// React.memo
+
+
+// import {useState} from 'react'; 
+// import React from 'react';
+
+// function App() {
+  
+
+//   return (
+//     <div>
+//      <HeaderFunction/>
+//      <Header title={"My name is Akshay"}></Header>
+
+//     </div>
+//   )
+// }
+
+// function HeaderFunction(){
+//   const [name,setName]= useState(" My name is Akshay")
+//   function UpdateName(){
+//     setName("My name is "+ Math.random());
+//   }
+//   return <div>
+// <button onClick={()=>{UpdateName()}} >Click here to change the title</button>
+// <Header title={name}></Header>
+// <Header title={"My name is Akshay"}></Header>
+// <Header title={"My name is Akshay"}></Header>
+// <Header title={"My name is Akshay"}></Header>
+// <Header title={"My name is Akshay"}></Header>
+//   </div>
+// }
+// const Header = React.memo(function Header ({title}){
+//   return (
+//   <div>
+//     {title}
+//     </div>
+//   )
+// })
+
+// export default App
+
+///////////////////Now we are going to learn the concept of key in react 
+
+
+import {useState} from 'react'; 
+import React from 'react';
+
+let count  = 4;
 function App() {
-  const [count, setCount] = useState(0)
+  const [applytodo,setTodo] = useState([{
+    id:1,
+    title:"go to gym",
+    description:"go to gym today"
+  },
+  {
+    id:2,
+    title:"eat food",
+    description:"eat your food today"
+  },
+  {
+    id:3,
+    title:"play game",
+    description:" gym today"
+  }
+])
+
+function AddTodo(){
+    setTodo([...applytodo,{
+      id:count++,
+      title:Math.random(),
+      description: Math.random()
+    }])
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={AddTodo}> Add new Todo</button>
+    {applytodo.map(todo=> <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+    <Todo  />
+    </div>
   )
 }
 
+function Todo({title,description}){
+  return<div>
+    <h1>{title}</h1>
+    <h5>{description}</h5>
+  </div>
+}
+
 export default App
+
+
